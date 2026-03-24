30ce3baf3484ff7666470a9fae4c3fa52ddcdf11
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-03-24 23:02:02 +0530

    Create HelloNameDefault.java

---
+++ HelloNameDefault.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        String name;
+        if (args.length > 0) {
+            name = args[0];
+        } else {
+            name = "World";
+        }
+        System.out.println("Hello " + name);
+    }
+}
