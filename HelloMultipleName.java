1cb8296034e4bd5e14ddce143358e3ef20a5632f
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-03-24 23:05:07 +0530

    Create HelloMultipleName.java

---
+++ HelloMultipleName.java
@@ -0,0 +1,12 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        if (args.length > 0) {
+            // Loop through all names
+            for (int i = 0; i < args.length; i++) {
+                System.out.println("Hello " + args[i]);
+            }
+        } else {
+            System.out.println("Hello World");
+        }
+    }
+}
