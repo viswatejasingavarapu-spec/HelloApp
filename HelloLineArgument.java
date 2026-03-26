44d1db092bdda1ab060884fc63a1b8d6ff6922ab
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-03-26 14:22:54 +0530

    Delete HelloLineArgument.java

--- HelloLineArgument.java
+++
@@ -1,9 +0,0 @@
-public class HelloLineArgument {
-    public static void main(String[] args) {
-        if (args.length > 0) {
-            System.out.println("Hello " + args[0]);
-        } else {
-            System.out.println("Hello World");
-        }
-    }
-}
