2eb88b18e3126c2759aca15fff0dacdeccce68f3
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-03-26 13:43:18 +0530

    Update HelloLineArgument.java

--- HelloLineArgument.java
+++ HelloLineArgument.java
@@ -1,4 +1,4 @@
-public class HelloWorld {
+public class HelloLineArgument {
     public static void main(String[] args) {
         if (args.length > 0) {
             System.out.println("Hello " + args[0]);
