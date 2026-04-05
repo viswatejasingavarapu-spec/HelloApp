6dd0aaf5699aab972bf52db3c5bba13f0ce70597
viswatejasingavarapu-spec <viswatejasingavarapu@gmail.com>
2026-04-05 22:18:27 +0530

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,11 +1,11 @@
 public class HelloWorld {
     public static void main(String[] args) {
-        if (args.length > 0) {
-            for (int i = 0; i < args.length; i++) {
-                System.out.println("Hello " + args[i]);
-            }
+        if (args.length == 0) {
+            System.out.println("Hello, World!");
         } else {
-            System.out.println("Hello World");
+            for (String name : args) {
+                System.out.println("Hello, " + name + "!");
+            }
         }
     }
 }
